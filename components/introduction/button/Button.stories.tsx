bf6d888a4481523869e3696a2afceab0fe5610fe
Daniel Vianna <daniel@cast-soft.com>
2021-11-27 21:42:52 -0500

    added figma - working

---
+++ components/introduction/button/Button.stories.tsx
@@ -0,0 +1,56 @@
+// Button.stories.ts|tsx
+
+import React from 'react';
+
+import { ComponentMeta } from '@storybook/react';
+
+import { Button } from './Button';
+
+import { withDesign } from 'storybook-addon-designs'
+
+export default {
+
+  title: 'Button',
+  component: Button,
+  decorators: [withDesign],
+
+  parameters: {
+    design: {
+      type: 'figma',
+      url:
+        'https://www.figma.com/file/Klm6pxIZSaJFiOMX5FpTul9F/storybook-addon-designs-sample',
+    },
+    backgrounds: {
+      values: [
+        { name: 'red', value: '#f00' },
+        { name: 'green', value: '#0f0' },
+        { name: 'blue', value: '#00f' },
+      ],
+    },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  primary: true,
+  label: 'Button',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Button',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Button',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Button',
+};
